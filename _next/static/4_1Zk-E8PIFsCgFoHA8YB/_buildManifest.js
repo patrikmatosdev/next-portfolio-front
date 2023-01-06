132648c47dcc88806b0e5ff2b0f7ce24915420bb
patrikmatosdev <53240137+patrikmatosdev@users.noreply.github.com>
2023-01-06 19:30:23 +0000

    Deploying to gh-pages from @ patrikmatosdev/next-portfolio-front@16cd6beff55c7f62e6dcadd897b943b9590bf5f9 🚀

---
+++ _next/static/4_1Zk-E8PIFsCgFoHA8YB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/259-e59469fc02e44ce5.js","static/chunks/pages/index-35f63b0bed646976.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
