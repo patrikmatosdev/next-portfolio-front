2678a4c7b858909ccbe2b1c3f0ddd7121e98b67e
patrikmatosdev <53240137+patrikmatosdev@users.noreply.github.com>
2023-01-06 20:03:36 +0000

    Deploying to gh-pages from @ patrikmatosdev/next-portfolio-front@06621e03426cd83eeec761189fecdbb1baf518c1 🚀

---
+++ _next/static/JuBjUU9N4z0Vt_2Prw0IY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/869-cf7f51fd819ff405.js","static/chunks/pages/index-e427b8dcd368236f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/eZKlL5JPdAeOIFoG66axD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/869-cf7f51fd819ff405.js","static/chunks/pages/index-fa2d20bf78ee47ac.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
