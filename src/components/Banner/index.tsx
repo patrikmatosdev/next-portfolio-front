58f08bc9afc4d43b2df84d0e315a15648eb5f7fb
patrik <patrikmatos.tecinfo@gmail.com>
2023-01-06 00:08:08 -0300

    fix: clean code and fix responsive banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,46 +1,27 @@
-import PageContainer from "../PageContainer";
-import Grid from "@mui/material/Grid";
-import Container from "./styles";
+import { Container, Hidden, TextContainer, DonwloadButton } from "./styles";
 import Image from "next/image";
 import BannerJpg from "../../assets/img/banner.jpg";
-import { Button } from "@mui/material";
 
 const Banner = () => {
   return (
     <Container>
       <Image fill alt={"banner-image"} src={BannerJpg} />
-      <Grid container style={{ position: "absolute" }}>
-        <Grid
+      <Hidden container>
+        <TextContainer
           direction="column"
           justifyContent="center"
           alignItems="center"
           container
-          style={{
-            height: "100vh",
-            background: "rgba(28,1,19,0.9)",
-          }}
         >
           <span>
             Olá, me chamo <strong>Patrik Matos</strong>.
           </span>
           <span>Sou um desenvolvedor front-end</span>
-          <Button
-            onClick={() => window.open("/curriculo.pdf")}
-            style={{
-              textTransform: "capitalize",
-              marginTop: 40,
-              background: "transparent",
-              color: "white",
-              border: "3px solid #fff",
-              padding: "15px 70px",
-              fontSize: "14pt",
-              height: "auto",
-            }}
-          >
+          <DonwloadButton onClick={() => window.open("curriculo.pdf")}>
             Baixar currículo
-          </Button>
-        </Grid>
-      </Grid>
+          </DonwloadButton>
+        </TextContainer>
+      </Hidden>
     </Container>
   );
 };
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,3 +1,4 @@
+import { Button, Grid } from "@mui/material";
 import { styled } from "@mui/system";
 
 const Container = styled("div")(() => {
@@ -20,7 +21,69 @@ const Container = styled("div")(() => {
       fontSize: "32pt",
       lineHeight: "36pt",
     },
+
+    "@media(max-width: 680px)": {
+      "& strong": {
+        fontSize: "24pt",
+        lineHeight: "20pt",
+      },
+
+      "& span": {
+        fontSize: "24pt",
+        lineHeight: "16pt",
+      },
+    },
+
+    "@media(max-width: 499px)": {
+      "& strong": {
+        fontSize: "16pt",
+        lineHeight: "20pt",
+      },
+
+      "& span": {
+        fontSize: "16pt",
+        lineHeight: "16pt",
+      },
+    },
   };
 });
 
-export default Container;
+const Hidden = styled(Grid)(() => {
+  return {
+    position: "absolute",
+  };
+});
+
+const TextContainer = styled(Grid)(() => {
+  return {
+    height: "100vh",
+    background: "rgba(28,1,19,0.9)",
+    padding: 15,
+  };
+});
+
+const DonwloadButton = styled(Button)(() => {
+  return {
+    textTransform: "capitalize",
+    marginTop: 40,
+    background: "transparent",
+    color: "white",
+    border: "3px solid #fff",
+    padding: "15px 70px",
+    fontSize: "14pt",
+    height: "auto",
+
+    "@media(max-width: 680px)": {
+      fontSize: "13pt",
+      lineHeight: "16pt",
+      width: "100%",
+      padding: "15px"
+    },
+
+    "@media(max-width: 499px)": {
+      fontSize: "12pt",
+    },
+  };
+});
+
+export { Container, DonwloadButton, Hidden, TextContainer };
